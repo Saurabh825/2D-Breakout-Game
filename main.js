52d6ded004f5d188b7217d0f1b0859c8d0a3ce12
sharma-jayprakash <178124387+sharma-jayprakash@users.noreply.github.com>
2025-04-07 00:51:00 +0530

    till activity 4 with proper functionin

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
+//creating a game variable ,and assining the width, height
+var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+  preload: preload, create: create, update: update
+});
 
-let ball; 
+//loading asset and priting them on screen ; 
+var ball; 
 
 
-//  using preload function of phaser , that loads the game every time before starting 
+
+
 function preload() {
-  //  in loading we have to set the scale  and background of canvas ,for which using show_All , a
-  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-  game.scale.pageAlignHorizontally = true;
-  game.scale.pageAlignVertically = true;
-  game.stage.backgroundColor ="#eee";
-  game.load.image("ball", "img/ball.png"); // adding image at the time of preloading 
+  //  to scale the canvas as full width and height without making its size ratio untouchable; 
+game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+game.scale.pageAlignHorizontally = true;
+game.scale.pageAlignVertically = true;
+// for making background; 
+game.stage.backgroundColor = '#eee';
+// using for loading the image in canvas ; 
+game.load.image('ball', './ball.png');
 }
 
-// adding the ball into the game ; 
+
 function create() {
-ball = game.add.sprite("50", "50", "ball") // first two arguments are the postion where you want to add the ball and the third one is assert name 
+  //after loading the image now rendering the image on canvas , for this use .add.sprit() function; 
+  //  The first two parameters are the top and left positions on the screen and
+  //  the third one is the name of the asset we defined earlier.
+  ball = game.add.sprite(40, 50, 'ball');
 }
-function update() {}
+
+//moving the ball into the screen; 
+function update() {
+  ball.x+=1
+  ball.y+=1
+}

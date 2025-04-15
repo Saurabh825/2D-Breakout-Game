7b810a664584660f7e7122b565241a6466cc5256
sharma-jayprakash <178124387+sharma-jayprakash@users.noreply.github.com>
2025-04-16 01:07:38 +0530

    till gameover

--- main.js
+++ main.js
@@ -28,23 +28,32 @@ function create(){
 
 ball = game.add.sprite(50, 50 , 'ball')
 //DISPLAYING paddle on the canvas; 
-paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle')
+paddle= game.add.sprite(game.world.width*0.5,game.world.height-5,'paddle'
+)
 //starting arcade physics in game ; 
 game.physics.startSystem(Phaser.Physics.ARCADE)
 //putting this thing(physic ) into the ball ; 
 game.physics.enable(ball,Phaser.Physics.ARCADE)
-game.physics.enable(paddle, Phaser.Physics.ARCADE);
+game.physics.enable(paddle,Phaser.Physics.ARCADE)
+
 
 //saying to take canvas ball as an boundary ; 
 ball.body.collideWorldBounds = true;
 //making ball bounce ; 
 ball.body.bounce.set(1)
-paddle.anchor.set(0.5,1);
+paddle.anchor.set(0.5,1)
 ball.body.velocity.set(150, 150)
+paddle.body.immovable=true; 
+
+game.physics.arcade.checkCollision.down=false; 
+ball.checkWorldBounds=true; 
+ball.events.onOutOfBounds.add(function(){
+  alert('Game over!');
+  location.reload();
+}, this);
 
 
 
-paddle.body.immovable = true;
 }
 
 
@@ -52,6 +61,7 @@ function update(){
   //used to update the position on the screen, 
   // ball.x+=1; 
   // ball.y+=1; 
-  game.physics.arcade.collide(ball, paddle);
-  paddle.x = game.input.x || game.world.width*0.5;
+game.physics.arcade.collide(ball, paddle)
+paddle.x = game.input.x || game.world.width*0.5;
+
 }
